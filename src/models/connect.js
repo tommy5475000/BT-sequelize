ba77df5de65b03b771430d5d55d0a724a4cab9e7
tommy547 <datcuongha@gmail.com>
2023-12-08 20:22:46 +0700

    up

--- src/models/connect.js
+++ src/models/connect.js
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize";
+import config from "../config/config.js";
 
-const sequelize = new Sequelize("db_node37", "root", "12345", {
-  host: "localhost",
-  port: "3307",
-  dialect: "mysql",
+const sequelize = new Sequelize(config.database, config.user, config.pass, {
+  host: config.host,
+  dialect: config.dialect,
+  port: config.port,
 });
 
 export default sequelize;
